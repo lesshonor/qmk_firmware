04d856824c5dae29487f29a51a0ea60e948a178f
honorless <86894501+lesshonor@users.noreply.github.com>
2023-02-22 21:45:31 -0500

    refactor: JSON keymaps, unnecessary configs

--- keyboards/ncrh/config.h
+++ keyboards/ncrh/config.h
@@ -14,8 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// config.h
-
 #pragma once
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
@@ -25,12 +23,4 @@
 #define LOCKING_RESYNC_ENABLE
 
 #define SOLENOID_PIN C14
-#define SOLENOID_ACTIVE true
 #define SOLENOID_DEFAULT_DWELL 20 
-
-#define VIA_EEPROM_CUSTOM_CONFIG_SIZE 7
-
-// VIA lighting is handled by the keyboard-level code
-#define VIA_CUSTOM_LIGHTING_ENABLE
-
-#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 8
