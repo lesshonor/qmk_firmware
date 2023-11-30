574daef9122fd3c3d6323ef7a54238177c9eac97
honorless <86894501+lesshonor@users.noreply.github.com>
2023-11-30 17:57:40 -0500

    refactor(mercutio): layouts & reformatting

--- keyboards/mechwild/mercutio/keymaps/default_625u_space/keymap.c
+++ keyboards/mechwild/mercutio/keymaps/default_625u_space/keymap.c
@@ -20,29 +20,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_625u_space(
                                                                                                                     KC_MUTE,
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        MO(1),            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
-        KC_LSFT, KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_RSFT,
+        MO(1),            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,             KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(2),            KC_RCTL ),
 
     [1] = LAYOUT_625u_space(
                                                                                                                     _______,
         KC_ESC,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        _______,          _______, KC_GRV,  KC_BSLS, _______, _______, _______, KC_LBRC, KC_RBRC, KC_SCLN, _______, KC_QUOT,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_SLSH,          KC_UP,
+        _______,          _______, KC_GRV,  KC_BSLS, _______, _______, _______, KC_LBRC, KC_RBRC, KC_SCLN,          KC_QUOT,
+        _______,          _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_SLSH,          KC_UP,
         _______, _______, _______,                   _______,                            KC_LEFT, KC_DOWN,          KC_RGHT ),
 
     [2] = LAYOUT_625u_space(
                                                                                                                     _______,
         _______,          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-        KC_CAPS,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______,                   _______,                            _______, _______,          _______ ),
-
-    [3] = LAYOUT_625u_space(
-                                                                                                                    _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        KC_CAPS,          _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______,                   _______,                            _______, _______,          _______ )
 };
 
@@ -50,7 +43,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [1] = { ENCODER_CCW_CW(_______, _______) },
-    [2] = { ENCODER_CCW_CW(_______, _______) },
-    [3] = { ENCODER_CCW_CW(_______, _______) },
+    [2] = { ENCODER_CCW_CW(_______, _______) }
 };
 #endif
